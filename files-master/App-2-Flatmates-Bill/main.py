0f64feebf2dfe53de0d594dc8fd86282323f2a3e
Chakri0712 <tmchakradhar2000@gmail.com>
2023-11-09 12:31:29 +0530

    App-2 proceedings

--- files-master/App-2-Flatmates-Bill/main.py
+++ files-master/App-2-Flatmates-Bill/main.py
@@ -1,40 +1,42 @@
 class Bill:
-
     """
     Bill class contains bill amount and bill period
     """
 
-    def __int__(self, amount, period):
+    def __init__(self, amount, period):
         self.amount = amount
         self.period = period
 
 
 class Flatmate:
-
     """
        Flatmate class contains name of the flatmate, no.of days he/she stayed in the house and
         method pays to know how much bill the flatmate needs to pay
        """
 
-    def __int__(self, name, days_in_house):
+    def __init__(self, name, days_in_house):
         self.name = name
         self.days_in_house = days_in_house
 
     def pays(self, bill):
-        pass
+        return ((bill.amount / 30) / 2) * self.days_in_house
 
 
 class PdfReport:
-
     """
        PdfReport class contains name of the pdf file and generate method which takes flatmates and bills
        as input to generate pdf
        """
 
-    def __int__(self, filename):
+    def __init__(self, filename):
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
         pass
 
 
+new_bill = Bill(int(input("What is the monthly bill? ")), "Monthly")
+Jack = Flatmate("Jack", int(input("How many days Jack stayed? ")))
+Anne = Flatmate("Anne", int(input("How many days Anne stayed? ")))
+print("Jack has to pay: " + str(Jack.pays(new_bill)))
+print("Anne has to pay: " + str(Anne.pays(new_bill)))
