6ccc4be1c6a71864b83fc4996c8c880125e7cc3d
chakradhar <tmchakradhar2000@gmail.com>
2023-11-10 12:35:08 +0530

    App-2: progressed on PDF generation

--- files-master/App-2-Flatmates-Bill/main.py
+++ files-master/App-2-Flatmates-Bill/main.py
@@ -1,3 +1,6 @@
+import fpdf
+
+
 class Bill:
     """
     Bill class contains bill amount and bill period
@@ -19,7 +22,7 @@ class Flatmate:
         self.days_in_house = days_in_house
 
     def pays(self, bill):
-        return ((bill.amount / 30) / 2) * self.days_in_house
+        return (bill.amount / sum(days_stayed)) * self.days_in_house
 
 
 class PdfReport:
@@ -32,11 +35,33 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        pdf = fpdf.FPDF()
+        pdf.add_page()
+
+        # Set the font for the heading.
+        pdf.set_font("Arial", "B", 16)
+
+        # Write the heading to the PDF.
+        pdf.multi_cell(100, 10, "Total Monthly Bill: " + str(bill.amount))
+
+        # Set the font for the body text.
+        pdf.set_font("Arial", "", 12)
+
+        # Write the monthly bill for Jack to the PDF.
+        pdf.multi_cell(100, 10, "Monthly Bill for Jack: " + str(flatmate1.pays(new_bill)))
+
+        # Write the monthly bill for Anne to the PDF.
+        pdf.multi_cell(100, 10, "Monthly Bill for Anne: " + str(flatmate2.pays(new_bill)))
+
+        # Output the PDF.
+        pdf.output(self.filename)
 
 
 new_bill = Bill(int(input("What is the monthly bill? ")), "Monthly")
-Jack = Flatmate("Jack", int(input("How many days Jack stayed? ")))
-Anne = Flatmate("Anne", int(input("How many days Anne stayed? ")))
+days_stayed = [int(input("How many days Jack stayed? ")), int(input("How many days Anne stayed? "))]
+Jack = Flatmate("Jack", days_stayed[0])
+Anne = Flatmate("Anne", days_stayed[1])
 print("Jack has to pay: " + str(Jack.pays(new_bill)))
 print("Anne has to pay: " + str(Anne.pays(new_bill)))
+pdf_report = PdfReport("Bill_Report.pdf")
+pdf_report.generate(Jack, Anne, new_bill)
