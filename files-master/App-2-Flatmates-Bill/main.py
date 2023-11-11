d1314b0ddb724607429270cb0bfa266be6bdeb7f
chakradhar <tmchakradhar2000@gmail.com>
2023-11-11 11:38:04 +0530

    Completed PDF generation and automatic opening of the PDF in the browser

--- files-master/App-2-Flatmates-Bill/main.py
+++ files-master/App-2-Flatmates-Bill/main.py
@@ -1,3 +1,4 @@
+import webbrowser
 import fpdf
 
 
@@ -22,7 +23,7 @@ class Flatmate:
         self.days_in_house = days_in_house
 
     def pays(self, bill):
-        return (bill.amount / sum(days_stayed)) * self.days_in_house
+        return round((bill.amount / sum(days_stayed)) * self.days_in_house)
 
 
 class PdfReport:
@@ -38,30 +39,56 @@ class PdfReport:
         pdf = fpdf.FPDF()
         pdf.add_page()
 
+        # adding image as an icon by giving path and dimensions
+        image_path = "files/house.png"
+        pdf.image(image_path, 10, 10, 20, 20)
+
+        # giving space in terms of height
+        pdf.ln(20)
+
+        # setting x-position for the header
+        pdf.set_x(50)
+
         # Set the font for the heading.
-        pdf.set_font("Arial", "B", 16)
+        pdf.set_font("Arial", "BU", 16)
 
         # Write the heading to the PDF.
-        pdf.multi_cell(100, 10, "Total Monthly Bill: " + str(bill.amount))
+        pdf.multi_cell(100, 10, "Monthly Bill")
 
         # Set the font for the body text.
         pdf.set_font("Arial", "", 12)
 
+        # setting margins for body
+        pdf.set_margins(50, 0, 50)
+
+        pdf.set_x(50)
+
+        pdf.multi_cell(100, 10, "Total Monthly Bill: " + str(bill.amount))
+
+        pdf.multi_cell(100, 10, "Period(in days): " + str(bill.period))
+
         # Write the monthly bill for Jack to the PDF.
-        pdf.multi_cell(100, 10, "Monthly Bill for Jack: " + str(flatmate1.pays(new_bill)))
+        pdf.multi_cell(100, 10,
+                       "Monthly Bill for " + Flatmate1.name + " for staying " + str(flatmate1.days_in_house) + " days: " + str(
+                           flatmate1.pays(new_bill)))
 
         # Write the monthly bill for Anne to the PDF.
-        pdf.multi_cell(100, 10, "Monthly Bill for Anne: " + str(flatmate2.pays(new_bill)))
+        pdf.multi_cell(100, 10,
+                       "Monthly Bill for " + Flatmate2.name + " for staying " + str(flatmate2.days_in_house) + " days: " + str(
+                           flatmate2.pays(new_bill)))
 
         # Output the PDF.
         pdf.output(self.filename)
 
+        # open pdf automatically
+        webbrowser.open(self.filename)
 
-new_bill = Bill(int(input("What is the monthly bill? ")), "Monthly")
+
+new_bill = Bill(int(input("What is the monthly bill? ")), 30)
 days_stayed = [int(input("How many days Jack stayed? ")), int(input("How many days Anne stayed? "))]
-Jack = Flatmate("Jack", days_stayed[0])
-Anne = Flatmate("Anne", days_stayed[1])
-print("Jack has to pay: " + str(Jack.pays(new_bill)))
-print("Anne has to pay: " + str(Anne.pays(new_bill)))
+Flatmate1 = Flatmate("Jack", days_stayed[0])
+Flatmate2 = Flatmate("Anne", days_stayed[1])
+print("Jack has to pay: " + str(Flatmate1.pays(new_bill)))
+print("Anne has to pay: " + str(Flatmate2.pays(new_bill)))
 pdf_report = PdfReport("Bill_Report.pdf")
-pdf_report.generate(Jack, Anne, new_bill)
+pdf_report.generate(Flatmate1, Flatmate2, new_bill)
