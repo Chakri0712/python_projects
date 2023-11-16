48b2661c3bdb10b15415ddcb80e1086632e3eb9c
Chakri0712 <tmchakradhar2000@gmail.com>
2023-11-16 19:30:18 +0530

    App-3 progression

--- files-master/App-3-Project-Math-Painting/main.py
+++ files-master/App-3-Project-Math-Painting/main.py
@@ -9,10 +9,13 @@ class Canvas:
         self.height = height
         self.color = color
 
-    def make(self, image_path):git add .__doc__ = 
-        data = np.zeros((self.height, self.width, 3), dtype=np.uint8)
-        data[:] = self.color
-        image = Image.fromarray(data, "RGB")
+        # create a numpy array of zeroes
+        self.data = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        # change the zeroes to color provided
+        self.data[:] = self.color
+
+    def make(self, image_path):
+        image = Image.fromarray(self.data, "RGB")
         # image = Image.new("RGB", (self.height, self.width), self.color)
         # draw = ImageDraw.Draw(image)
         # draw.rectangle((100, 100, 200, 200), fill="blue")
@@ -28,7 +31,8 @@ class Square:
         self.color = color
 
     def draw(self, canvas):
-        pass
+        canvas.data[self.x: self.x + self.side, self.y:self.y + self.side] = self.color
+        canvas.make("Image.png")
 
 
 class Rectangle:
@@ -41,9 +45,23 @@ class Rectangle:
         self.color = color
 
     def draw(self, canvas):
-        pass
+        canvas.data[self.x: self.x + self.height, self.y:self.y + self.width] = self.color
+        canvas.make("Image.png")
 
 
-canvas = Canvas(5000, 5000, [200, 20, 20])
+canvas_width = int(input("Enter the width of Canvas: "))
+canvas_height = int(input("Enter the height of Canvas: "))
+canvas = Canvas(500, 500, [200, 20, 20])
 # canvas = Canvas(5000, 5000, "Red")
 canvas.make("Image.png")
+square = Square(100, 150, 75, [50, 50, 200])
+square.draw(canvas)
+rect = Rectangle(200, 300, 50, 75, [0, 250, 50])
+rect.draw(canvas)
+
+# ask user for canvas width, height, color in list format (RGB)
+#
+# if user chooses square, then ask for x, y, side, color in list format (RGB)
+#
+# if user chooses rect, then ask for x, y, width, height, color in list format (RGB)
+
